6f87f9c13f5d8714cffe286062c271a2fb6759be
jucema179 <45674796+jucema179@users.noreply.github.com>
2020-12-10 23:23:30 -0600

    first commit

---
+++ precache-manifest.74b5d09cd5408e30f6b0c2f964c63814.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59012c3b9f78c0ac0802f457ad1fc664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c5b8ed7524059219c08",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "e611a0d7e1342b29b925",
+    "url": "/static/css/main.44d67959.chunk.css"
+  },
+  {
+    "revision": "9c5b8ed7524059219c08",
+    "url": "/static/js/2.112de8fe.chunk.js"
+  },
+  {
+    "revision": "e611a0d7e1342b29b925",
+    "url": "/static/js/main.9d30fab7.chunk.js"
+  },
+  {
+    "revision": "5313967250b4293df7aa",
+    "url": "/static/js/runtime-main.0bc9d8d3.js"
+  }
+]);
